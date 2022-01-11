8b0e82a97c6e29a2ebed53e4a96c9d9e9fd2236b
Matías Fuentes <matiasf.cortez@gmail.com>
2022-01-11 02:56:39 -0300

    added render products in main page, added in case of images error images replacement

--- src/js/getCategories.js
+++ src/js/getCategories.js
@@ -3,10 +3,9 @@ import { getCategories } from '../constants/index';
 const dropdownCategories = document.getElementById('dropdown-categories');
 
 const categoryTemplate = (category, id) =>
-	`<a href="/" id="${id}" class="bg-white px-4 py-2 block hover:bg-slate-300">${category}</a>`;
+	`<a id="${id}" class="bg-white cursor-pointer px-4 py-2 block hover:bg-slate-300">${category}</a>`;
 
 fetch(getCategories).then((data) => data.json()).then((data) => {
-	console.log(data);
 	data.forEach((el) => {
 		const { name, id } = el;
 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 import './js/getCategories';
+import { renderProducts } from './js/renderProducts';
