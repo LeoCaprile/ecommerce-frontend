9c075230a5c8ef37bb20051d5c6fabbdbcc057ba
Matías Fuentes <matiasf.cortez@gmail.com>
2022-01-11 01:07:36 -0300

    Navbar dropdown categories fixed

--- src/js/getCategories.js
+++ src/js/getCategories.js
@@ -3,7 +3,7 @@ import { getCategories } from '../constants/index';
 const dropdownCategories = document.getElementById('dropdown-categories');
 
 const categoryTemplate = (category, id) =>
-	`<a href="/" id="${id}" class="bg-white px-4 py-2 hover:bg-slate-300">${category}</a>`;
+	`<a href="/" id="${id}" class="bg-white px-4 py-2 block hover:bg-slate-300">${category}</a>`;
 
 fetch(getCategories).then((data) => data.json()).then((data) => {
 	console.log(data);
@@ -13,6 +13,6 @@ fetch(getCategories).then((data) => data.json()).then((data) => {
 		const category = document.createElement('div');
 		category.innerHTML = categoryTemplate(name, id);
 
-		dropdownCategories.append(category);
+		dropdownCategories.append(category.firstChild);
 	});
 });
