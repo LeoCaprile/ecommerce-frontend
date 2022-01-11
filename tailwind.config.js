2d99666f97bee46a37990fd1970142e47b8fe1c7
Matías Fuentes <matiasf.cortez@gmail.com>
2022-01-11 03:45:36 -0300

    fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	mode: 'jit',
-	content: [ './src/index.html', './src/**/*.{html,js}' ],
+	content: [ './index.html', './src/**/*.{html,js}' ],
 	darkMode: false,
 	theme: {
 		extend: {}
