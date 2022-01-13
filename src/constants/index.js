1a77b5b3fee867a25d3dad8d449e43288d39e93c
Matías Fuentes <matiasf.cortez@gmail.com>
2022-01-12 23:28:02 -0300

    fetch links to changed to  production

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const getCategories = 'https://ecommerce-backend-r4ajj2pf3-leocaprile.vercel.app/api/getCategories';
+export const getCategories = 'https://ecommerce-backend-amber.vercel.app/api/getCategories';
 export const getProducts = (name = '', category = '', price = '') =>
-	`https://ecommerce-backend-r4ajj2pf3-leocaprile.vercel.app/api/getProducts?name=${name}&category=${category}&price=${price}`;
-export const getDiscounts = 'https://ecommerce-backend-nvwgy5azt-leocaprile.vercel.app/api/getDiscounts';
+	`https://ecommerce-backend-amber.vercel.app/api/getProducts?name=${name}&category=${category}&price=${price}`;
+export const getDiscounts = 'https://ecommerce-backend-amber.vercel.app/api/getDiscounts';
