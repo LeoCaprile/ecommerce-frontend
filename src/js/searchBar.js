e9bc62152f8efe50bf50908e9f7ba560788b5778
Matías Fuentes <matiasf.cortez@gmail.com>
2022-01-12 22:58:51 -0300

    added mobile design

--- src/js/searchBar.js
+++ src/js/searchBar.js
@@ -2,6 +2,7 @@ import { renderProducts } from './renderProducts';
 const searchBar = document.getElementById('search-bar');
 
 searchBar.addEventListener('keydown', (e) => {
+	e.preventDefault();
 	if (e.code !== 'Enter') return;
 
 	const searchText = e.target.value;
